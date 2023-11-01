d1847d790d4e2f510436916b36c6d5fc3395e1e9
jt-96 <jetorrico@outlook.com>
2023-11-01 17:30:17 -0300

    feature: add landing page template

---
+++ client/src/pages/Landing/Landing.page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LandingPage() {
+
+
+    return (
+
+        <>
+            <h1>This is Landing Page</h1>
+        </>
+    )
+}
+
+export default LandingPage;
