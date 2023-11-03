477ad79ccdb1842e59b1d77ed0c2b58af689e61b
jt-96 <jetorrico@outlook.com>
2023-11-03 17:42:55 -0300

    fix: fix sticky navbar

--- client/src/components/NavBar/navbar.component.jsx
+++ client/src/components/NavBar/navbar.component.jsx
@@ -67,8 +67,8 @@ function NavBarComponent() {
 
     return (
 
-        <div className="max-h-[768px] w-[calc(100%)] overflow-none">
-            <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+        <div className="sticky top-0 z-10 max-h-[768px] w-[calc(100%)] overflow-none">
+            <Navbar className="h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
                 <div className="flex items-center justify-between text-blue-gray-900">
                     <img
                         className="h-24 w-24 rounded-full object-cover object-center"
