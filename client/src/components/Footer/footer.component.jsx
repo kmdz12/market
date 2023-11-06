5fe89296938f5c9b1dd806849915c6db1342bda7
jt-96 <jetorrico@outlook.com>
2023-11-06 16:39:12 -0300

    feature: add footer component template

---
+++ client/src/components/Footer/footer.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function FooterComponent () {
+
+    return (
+
+        //Main Container
+        <div className="md:container md:mx-auto">
+
+            {/* Name & Logo Container */}
+            <div>
+
+            </div>
+
+            {/* Links Container */}
+            <div>
+
+            </div>
+
+            {/* Socials Container */}
+            <div>
+
+            </div>
+
+            {/* Copyright/Credits Container */}
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default FooterComponent;
