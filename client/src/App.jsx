20eb28a2c4eb1bb600b9893dfda55cf2aa21142e
jt-96 <jetorrico@outlook.com>
2023-11-07 14:08:45 -0300

    feature: add routes for coordinator and user login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { Switch, Route } from 'wouter';
 import LandingPage from './pages/Landing/Landing.page';
+import UserloginPage from './pages/UserLogin/userlogin.page';
+import CoordinatorLoginPage from './pages/CoordinatorLogin/coordinator.page';
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
     <>
       <Switch>
         <Route path="/" component={LandingPage} />
+        <Route path="/login" component={UserloginPage} />
+        <Route path="/coordinator/admin/login" component={CoordinatorLoginPage} />
       </Switch>
     </>
   )
