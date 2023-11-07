00bfc09d0b8a7850d7c802c03bcfeb75f1cc259b
jt-96 <jetorrico@outlook.com>
2023-11-07 14:23:17 -0300

    feature: add CoordinatorLogin and user register page template

---
+++ client/src/pages/CoordinatorLogin/CoordinatorLogin.page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function CoordinatorLoginPage() {
+
+    return (
+        <h1>This is Coordinator Login Page!</h1>
+    )
+}
+
+export default CoordinatorLoginPage;
---
+++ client/src/pages/UserRegister/UserRegister.page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function UserRegisterPage() {
+
+    return (
+        <h1>This is UserRegisterPage!</h1>
+    )
+}
+
+export default UserRegisterPage;
