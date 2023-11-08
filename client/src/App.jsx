6b982eabaa9d3196c3533ab3325bcc28d450703e
jt-96 <jetorrico@outlook.com>
2023-11-08 20:31:41 -0300

    feature: add redirect to main page on unknown paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
-import { Switch, Route } from 'wouter';
+import { Switch, Route, Redirect } from 'wouter';
 import LandingPage from './pages/Landing/Landing.page';
 import UserLoginPage from './pages/UserLogin/UserLogin.page';
 import CoordinatorLoginPage from './pages/CoordinatorLogin/CoordinatorLogin.page';
 import UserRegisterPage from './pages/UserRegister/UserRegister.page';
+import UnknownPage from './pages/unknownPage/Unknown.page';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" component={UserLoginPage} />
         <Route path="/register" component={UserRegisterPage} />
         <Route path="/coordinator/admin/login" component={CoordinatorLoginPage} />
+        <Route path="/:rest*">{() => <Redirect to='/' />}</Route>
       </Switch>
     </>
   )
