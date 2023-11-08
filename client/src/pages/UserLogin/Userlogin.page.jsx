82bb6f2ac486ea2b539cde43e5c1c8ecdf0c0e03
jt-96 <jetorrico@outlook.com>
2023-11-08 16:59:00 -0300

    refactor: import case

--- client/src/pages/UserLogin/Userlogin.page.jsx
+++ client/src/pages/UserLogin/Userlogin.page.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import { Link } from 'wouter';
+import { Typography, Card, Input, Button } from '@material-tailwind/react';
 
-function UserloginPage() {
+function UserLoginPage() {
 
     return (
-        <h1>This is User Login Page!</h1>
+        <div>
+            {/* Background Image Container */}
+            <div className="w-screem h-screen bg-[url('./images/login.jpg')] bg-auto bg-no-repeat bg-center absolute inset-0 blur-sm"></div>
+            {/* Form Container */}
+            <div className="absolute inset-0">
+                <div className="h-full w-full flex justify-center items-center">
+                    <div className='h-1/2 w-4/5 md:w-1/4 md:h-3/6'>
+                        <div className='bg-gray-300 flex justify-center items-center rounded'>
+                            <Card color="transparent" shadow={false}>
+                                <Typography variant="h4" color="blue-gray" className='text-center text-base my-5'>
+                                    Bienvenido!
+                                </Typography>
+                                <form className="my-5 mx-3 max-w-screen-lg">
+                                    <div className="mb-1 flex flex-col gap-6">
+                                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                                            Email
+                                        </Typography>
+                                        <Input
+                                            className="w-full"
+                                            variant="standard"
+                                            color="pink"
+                                            size="lg"
+                                            placeholder="Ingresa tu correo"
+                                        />
+                                        <Typography variant="h6" color="blue-gray" className="-mb-3">
+                                            Contraseña
+                                        </Typography>
+                                        <Input
+                                            type="password"
+                                            variant="standard"
+                                            color="pink"
+                                            size="lg"
+                                            placeholder="Ingresa tu contraseña"
+                                        />
+                                    </div>
+                                    <Button className="mt-6" fullWidth color="pink">
+                                        Iniciar Sesion!
+                                    </Button>
+                                    <Typography color="gray" className="mt-4 text-center font-normal">
+                                        No tenes una cuenta?{" "}
+                                        <Link href="/login">
+                                            <Typography as={'a'} className="font-medium text-blue-900 inline-block">
+                                                Registrate!
+                                            </Typography>
+                                        </Link>
+                                    </Typography>
+                                </form>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default UserloginPage;
+export default UserLoginPage;
