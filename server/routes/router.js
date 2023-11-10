6e0236890c9d2b2ca9e0b8b1b32a133c854512d5
jt-96 <jetorrico@outlook.com>
2023-11-10 15:57:04 -0300

    feature: add authController import, add route for register

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { authController } = require('../controller/auth/AuthController.js');
+
+router.route('/register')
+    .post(authController.register)
 
 module.exports = router;
