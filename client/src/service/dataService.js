76e1baac81ecb0dfee02ee76b80033be8ebcef6a
jt-96 <jetorrico@outlook.com>
2023-11-10 16:52:45 -0300

    feature: add userlogin method

--- client/src/service/dataService.js
+++ client/src/service/dataService.js
@@ -12,7 +12,11 @@ class DataService {
     }
 
     userRegistration = (email, password) => {
-        return this.service.post('/register', {email, password}).then((response) => response.data).catch((error) => error.response.data)
+        return this.service.post('/register', { email, password }).then((response) => response.data).catch((error) => error.response.data);
+    }
+
+    userLogin = (email, password) => {
+        return this.service.post('/login', { email, password }).then((response) => response.data).catch((error) => error.response.data);
     }
 }
 
