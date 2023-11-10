33efa0060f63f559282b0c2e6d9c817da3e8b004
jt-96 <jetorrico@outlook.com>
2023-11-10 15:55:29 -0300

    feature: add login functionality, add alert control, add form control

--- client/src/pages/UserRegister/UserRegister.page.jsx
+++ client/src/pages/UserRegister/UserRegister.page.jsx
@@ -1,14 +1,76 @@
-import React from 'react';
-import { Link } from 'wouter';
-import { Typography, Card, Input, Button } from '@material-tailwind/react';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'wouter';
+import { Typography, Card, Input, Button, Alert } from '@material-tailwind/react';
+import DataService from '../../service/dataService';
 import './UserRegister.style.css';
 
 function UserRegisterPage() {
+
+    const [location, setLocation] = useLocation();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [alertStatus, setAlertStatus] = useState({
+        code: 0,
+        message: '',
+        show: false
+    })
+
+    const dataService = new DataService();
+
+    function handleEmail(e) {
+        const { value } = e.target;
+        setEmail(value);
+    }
+
+    function handlePassword(e) {
+        const { value } = e.target;
+        setPassword(value);
+    }
+
+    function handleRegister(e) {
+        e.preventDefault();
+
+        if (email.match("([A-Za-z0-9][._]?)+[A-Za-z0-9]@[A-Za-z0-9]+(\.?[A-Za-z0-9]){2}\.(com?|net|org)+(\.[A-Za-z0-9]{2,4})?") && password.match("[0-9a-zA-Z]{8,}")) {
+
+            dataService.userRegistration(email, password).then((response) => {
+                if (response.code === 200) {
+                    setAlertStatus({ code: 200, message: 'Registro Completado, ahora te redigiremos para loguearte!', show: true })
+
+                    setTimeout(() => {
+                        setAlertStatus((prevValue) => ({
+                            ...prevValue,
+                            show: false
+                        }))
+                        setLocation("/login")
+                    }, 5000);
+
+                } else if (response.code === 302) {
+                    setAlertStatus({ code: 302, message: 'Este correo ya esta en uso!', show: true })
+
+                    setTimeout(() => {
+                        setAlertStatus((prevValue) => ({
+                            ...prevValue,
+                            show: false
+                        }))
+                    }, 5000);
+                }
+            })
+        }
+    }
 
     return (
         <div>
             {/* Background Image Container */}
             <div className="w-screem h-screen register bg-auto bg-no-repeat bg-center absolute inset-0 blur-sm"></div>
+            {
+                alertStatus.show ?
+
+                    <div>
+                        <Alert className="flex justify-center" variant="gradient" color={alertStatus.code === 200 ? "green" : "red"} open={alertStatus.show} animate={{ mount: { y: 0 }, unmount: { y: -100 } }}>{alertStatus.message}</Alert>
+                    </div>
+                    :
+                    null
+            }
             {/* Form Container */}
             <div className="absolute inset-0">
                 <div className="h-full w-full flex justify-center items-center">
@@ -18,17 +80,20 @@ function UserRegisterPage() {
                                 <Typography variant="h4" color="blue-gray" className='text-center text-base my-5'>
                                     Registrate, es gratis!
                                 </Typography>
-                                <form className="my-5 mx-3 max-w-screen-lg">
+                                <form className="my-5 mx-3 max-w-screen-lg" onSubmit={handleRegister}>
                                     <div className="mb-1 flex flex-col gap-6">
                                         <Typography variant="h6" color="blue-gray" className="-mb-3">
                                             Tu email
                                         </Typography>
                                         <Input
-                                            className="w-full"
                                             variant="standard"
                                             color="pink"
                                             size="lg"
                                             placeholder="nombre@email.com"
+                                            onChange={handleEmail}
+                                            type='email'
+                                            required
+                                            pattern="([A-Za-z0-9][._]?)+[A-Za-z0-9]@[A-Za-z0-9]+(\.?[A-Za-z0-9]){2}\.(com?|net|org)+(\.[A-Za-z0-9]{2,4})?"
                                         />
                                         <Typography variant="h6" color="blue-gray" className="-mb-3">
                                             Contraseña
@@ -39,9 +104,13 @@ function UserRegisterPage() {
                                             color="pink"
                                             size="lg"
                                             placeholder="********"
+                                            onChange={handlePassword}
+                                            required
+                                            pattern="[0-9a-zA-Z]{8,}"
                                         />
+                                        <Typography variant="small" className="text-center">Contraseña debe tener minimo 8 caracteres</Typography>
                                     </div>
-                                    <Button className="mt-6" fullWidth color="pink">
+                                    <Button className="mt-6" fullWidth color="pink" type='submit'>
                                         Registrame!
                                     </Button>
                                     <Typography color="gray" className="mt-4 text-center font-normal">
