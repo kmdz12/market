c11a0f0956ccd142014c4172329b19d604a51523
jt-96 <jetorrico@outlook.com>
2023-11-13 16:12:43 -0300

    refactor: add responses on conditionals

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -5,14 +5,15 @@ const verifyJWT = (req, res) => {
     const token = req.headers['x-access-token'];
 
     if (!token) {
-        res.send('Token is missing!');
+        res.json({ auth: false, message: 'Token is missing!' });
 
     } else {
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
 
             if (err) {
-                res.json({ auth: false, message: 'Failed to authenticate!' });
+                res.json({ auth: false, message: err.message });
             } else {
+                res.json({ auth: true, message: 'User Authenticated!' });
                 req.userId = decoded.id;
             }
         })
