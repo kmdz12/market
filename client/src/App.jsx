0cad6f3e12580648aa022712849e66dbc6a63107
jt-96 <jetorrico@outlook.com>
2023-11-14 16:17:04 -0300

    feature: include admin panel route and component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from './pages/Landing/Landing.page';
 import UserLoginPage from './pages/UserLogin/UserLogin.page';
 import CoordinatorLoginPage from './pages/CoordinatorLogin/CoordinatorLogin.page';
 import UserRegisterPage from './pages/UserRegister/UserRegister.page';
+import CoordinatorPanelPage from './pages/CoordinatorPanel/CoordinatorPanel.page';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" component={UserLoginPage} />
         <Route path="/register" component={UserRegisterPage} />
         <Route path="/coordinator/admin/login" component={CoordinatorLoginPage} />
+        <Route path="/coordinator/admin/panel" component={CoordinatorPanelPage} />
         <Route path="/:rest*">{() => <Redirect to='/' />}</Route>
       </Switch>
     </>
