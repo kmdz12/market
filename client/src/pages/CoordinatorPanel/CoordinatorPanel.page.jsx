e95a426cdde91314218c803dd58ff2cbed68ee42
jt-96 <jetorrico@outlook.com>
2023-11-14 17:41:08 -0300

    feature: add cards for each crud

--- client/src/pages/CoordinatorPanel/CoordinatorPanel.page.jsx
+++ client/src/pages/CoordinatorPanel/CoordinatorPanel.page.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'wouter';
 import AdminNavbarComponent from '../../components/AdminNavbar/AdminNavbar.component';
 import DataService from '../../service/dataService';
+import { Card, CardBody, CardFooter, Button, Typography } from '@material-tailwind/react';
 
 function CoordinatorPanelPage() {
 
@@ -15,7 +16,7 @@ function CoordinatorPanelPage() {
             const token = localStorage.getItem('token');
 
             dataService.checkLoggedUser().then((response) => {
-                
+
                 if (response.user !== import.meta.env.VITE_OWNER_EMAIL && response.user !== import.meta.env.VITE_SUBOWNER_EMAIL && response.user !== import.meta.env.VITE_ENTERPRISE_EMAIL) {
                     setLocation('/')
                 }
@@ -35,9 +36,57 @@ function CoordinatorPanelPage() {
     }, [])
 
     return (
-        <>
+        <div className='bg-white pb-5'>
             <AdminNavbarComponent />
-        </>
+            <div className='md:container md:mx-auto flex justify-center py-12 mt-10'>
+                <Typography variant="h2" className='text-center'>Panel de Administracion</Typography>
+            </div>
+
+            <div className='md:container md:mx-auto flex justify-center items-center flex-col lg:flex-row h-fit'>
+                <Card className="mt-2 w-80 border lg:mx-2 md:w-full">
+                    <CardBody>
+                        <Typography variant="h5" color="blue-gray" className="mb-2">
+                            Ordenes ABM
+                        </Typography>
+                        <Typography variant="small">
+                            Aqui podras ver todas las ordenes generadas y su estado.
+                        </Typography>
+                    </CardBody>
+                    <CardFooter className="pt-0 flex justify-center">
+                        <Button>INGRESAR</Button>
+                    </CardFooter>
+                </Card>
+
+                <Card className="mt-2 w-80 border lg:mx-2 md:w-full">
+                    <CardBody>
+                        <Typography variant="h5" color="blue-gray" className="mb-2">
+                            Productos ABM
+                        </Typography>
+                        <Typography variant="small">
+                            Aqui podras ver la lista completa de productos.
+                        </Typography>
+                    </CardBody>
+                    <CardFooter className="pt-0 flex justify-center">
+                        <Button>INGRESAR</Button>
+                    </CardFooter>
+                </Card>
+
+                <Card className="mt-2 w-80 border lg:mx-2 md:w-full">
+                    <CardBody>
+                        <Typography variant="h5" color="blue-gray" className="mb-2">
+                            Usuarios ABM
+                        </Typography>
+                        <Typography variant="small">
+                            Aqui podras ver todos los usuarios registrados.
+                        </Typography>
+                    </CardBody>
+                    <CardFooter className="pt-0 flex justify-center">
+                        <Button>INGRESAR</Button>
+                    </CardFooter>
+                </Card>
+            </div>
+            
+        </div>
     )
 }
 
