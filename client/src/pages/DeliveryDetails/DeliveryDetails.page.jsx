f5bcd6860e7c4f52e272b5c46ee668f37ea3225b
jt-96 <jetorrico@outlook.com>
2023-12-01 16:52:09 -0300

    feature: add context, add state to load user details, cart products, add hook to check user log, load cart from localStorage, add handler for pickup form, fix height of main div to use all available space, map cart items, subtotal, total and payment in cart section, small button style change

--- client/src/pages/DeliveryDetails/DeliveryDetails.page.jsx
+++ client/src/pages/DeliveryDetails/DeliveryDetails.page.jsx
@@ -1,17 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { Input, Typography, Select, Option, Textarea, Button } from '@material-tailwind/react';
 import NavBarComponent from '../../components/NavBar/navbar.component';
 import FooterComponent from '../../components/Footer/footer.component';
-import { Input, Typography, Select, Option, Textarea, Button } from '@material-tailwind/react';
+import DataService from '../../service/dataService';
+import CartContext from '../../store/CartContext';
 
 function DeliveryDetailsPage() {
 
+    const cartCTX = useContext(CartContext);
+    const [cartProducts, setCartProducts] = useState({});
+    const [userLogged, setUserLogged] = useState();
+    const [currentUser, setCurrentUser] = useState({
+        email: '',
+        id: 0,
+        info_id: 0,
+        name: '',
+        phone: '',
+        surname: ''
+    });
+    const [pickup, setPickup] = useState({
+        name: '',
+        surname: ''
+    })
+    const dataService = new DataService();
+
+    useEffect(() => {
+        if (localStorage.getItem('token') !== null) {
+
+            dataService.checkLoggedUser().then((response) => {
+                setUserLogged(response)
+            }).catch((e) => {
+                setUserLogged(e.response.data.loggedIn)
+                localStorage.removeItem('token');
+                setLocation('/login')
+            })
+        } else {
+            setLocation('/login')
+        }
+
+    }, [])
+
+    useEffect(() => {
+        dataService.getUser().then((response) => setCurrentUser(response))
+        setPickup({
+            name: currentUser.name,
+            surname: currentUser.surname
+        })
+    }, [userLogged])
+
+    useEffect(() => {
+
+        const local = JSON.parse(localStorage.getItem('cart'));
+
+        if (local) {
+            setCartProducts(local)
+        } else {
+            return;
+        }
+
+    }, [cartCTX])
+
+    useEffect(() => {
+
+        // console.log(currentUser, userLogged)
+        // console.log(cartProducts)
+    }, [currentUser, userLogged])
+
+    function handlePickupChange(e) {
+        const { name, value } = e.target;
+
+        setPickup((prevValue) => {
+            return {
+                ...prevValue,
+                [name]: value
+            }
+        })
+    }
+
     return (
         <>
-            <div className='h-min-screen'>
+            <div className='flex flex-col h-screen'>
                 <NavBarComponent />
-                <div className='h-full p-2 background lg:flex lg:justify-center'>
+                <div className='p-2 background lg:flex lg:justify-center grow'>
                     {/* Final Cart Details */}
-                    <div className='flex flex-col my-4 bg-gray-300 p-5'>
+                    <div className='flex flex-col my-4 bg-gray-300 p-5 lg:w-1/3'>
                         <Typography variant="h2" className='text-center'>Tu Carrito</Typography>
                         <div className='flex justify-between mt-5'>
                             <div className='flex flex-col'>
@@ -19,67 +91,65 @@ function DeliveryDetailsPage() {
                             </div>
                             <Typography variant="lead" >Subtotal</Typography>
                         </div>
-                        <div className='flex justify-between my-2'>
-                            <div className='flex flex-col'>
-                                <Typography variant="lead" >Fresh fruits and vegetables</Typography>
-                                <Typography variant="lead" >Cantidad: 1</Typography>
-                            </div>
-                            <Typography variant="lead" >$12.32</Typography>
-                        </div>
-                        <div className='flex justify-between my-2'>
-                            <div className='flex flex-col'>
-                                <Typography variant="lead" >Fresh fruits and vegetables</Typography>
-                                <Typography variant="lead" >Cantidad: 1</Typography>
-                            </div>
-                            <Typography variant="lead" >$12.32</Typography>
-                        </div>
-                        <div className='flex justify-between my-2'>
-                            <div className='flex flex-col'>
-                                <Typography variant="lead" >Fresh fruits and vegetables</Typography>
-                                <Typography variant="lead" >Cantidad: 1</Typography>
-                            </div>
-                            <Typography variant="lead" >$12.32</Typography>
-                        </div>
-                        <div className='flex justify-between my-2'>
-                            <div className='flex flex-col'>
-                                <Typography variant="lead" >Fresh fruits and vegetables</Typography>
-                                <Typography variant="lead" >Cantidad: 1</Typography>
-                            </div>
-                            <Typography variant="lead" >$12.32</Typography>
-                        </div>
+                        {
+                            cartProducts.items?.map((item, index) => (
+
+                                <div className='flex justify-between my-2' key={index}>
+                                    <div className='flex flex-col'>
+                                        <Typography variant="lead" >{item.name}</Typography>
+                                        <Typography variant="lead" >Cantidad: {item.quantity}</Typography>
+                                    </div>
+                                    <Typography variant="lead" >${item.quantity * item.price}</Typography>
+                                </div>
+                            ))
+                        }
+
                         <div className='flex flex-col justify-between mt-5 grow lg:justify-end'>
                             <div className='flex justify-between'>
                                 <Typography variant='lead'>Metodo de Pago:</Typography>
-                                <Typography variant='lead'>Transferencia Bancaria</Typography>
+                                {
+                                    cartProducts.paymentType == 0 ?
+
+                                        <Typography variant='lead'>Transferencia Bancaria</Typography>
+
+                                        :
+
+                                        <Typography variant='lead'>Mercado Pago</Typography>
+
+                                }
                             </div>
 
                             <div className='flex justify-between'>
                                 <Typography variant='lead'>Total:</Typography>
-                                <Typography variant='h5'>$ 1203.21</Typography>
+                                <Typography variant='h5'>$ {cartProducts.total}</Typography>
                             </div>
                         </div>
                     </div>
 
                     {/* Delivery Details */}
-                    <div className='flex flex-col my-4 bg-gray-300 p-5 lg:w-1/2'>
-                        <Typography variant="h2" className='text-center'>Informacion de Delivery</Typography>
-                        <Typography variant='paragraph' className='text-center mt-5'>A continuacion, provee los detalles para el envio de tu pedido, o selecciona uno previamente guardado!</Typography>
+                    <div className='flex flex-col my-4 bg-gray-300 p-5 lg:w-1/3'>
+                        <div>
+                            <Typography variant="h2" className='text-center'>Informacion de Delivery</Typography>
+                            <Typography variant='paragraph' className='text-center mt-5'>A continuacion, provee los detalles para el envio de tu pedido, o selecciona uno previamente guardado!</Typography>
+                        </div>
 
                         {/* Form Container */}
-                        <div>
+                        <div className='flex flex-col grow'>
                             <Typography variant='paragraph' className='text-center mt-5'>Quien retira el envio?</Typography>
                             <div className='flex flex-col items-center xl:flex-row'>
                                 <div className='w-full p-3'>
-                                    <Input label='Nombre' className='bg-white' />
+                                    <Input label='Nombre' className='bg-white' name='name' value={pickup.name} onChange={handlePickupChange} />
                                 </div>
                                 <div className='w-full p-3'>
-                                    <Input label='Apellido' className='bg-white' />
+                                    <Input label='Apellido' className='bg-white' name='surname' value={pickup.surname} onChange={handlePickupChange} />
                                 </div>
                             </div>
 
                             {/* My Directions Container */}
-                            <div className='bg-gray-300 flex flex-col items-center rounded my-3 lg:items-start lg:m-0'>
-                                <Typography variant='h5' className='p-3'>Mis Direcciones</Typography>
+                            <div className='bg-gray-300 flex flex-col items-center rounded my-3 lg:items-start lg:m-0 grow'>
+                                <div>
+                                    <Typography variant='h5' className='p-3'>Mis Direcciones</Typography>
+                                </div>
                                 {/* Saved Directions Container*/}
                                 <div className='flex flex-row overflow-x-scroll py-2 w-full px-4'>
                                     <div className='bg-gray-500 rounded p-3 mx-1'>
@@ -115,7 +185,7 @@ function DeliveryDetailsPage() {
                                     </div>
                                 </div>
                                 {/* New Direction Container */}
-                                <div className='py-3 w-full px-4'>
+                                <div className='flex flex-col py-3 w-full px-4 grow'>
                                     <div className='md:flex md:justify-between'>
                                         <div className='py-2 md:w-full md:mr-1'>
                                             <Select label="Departamento" className='bg-white'>
@@ -145,8 +215,8 @@ function DeliveryDetailsPage() {
                                         </div>
                                     </div>
 
-                                    <div className='flex md:flex-col md:items-center'>
-                                        <Button className='flex grow justify-center shadow-pop-br' color='pink' variant="gradient">Finalizar Orden</Button>
+                                    <div className='flex justify-center md:flex-col md:items-end grow md:justify-end mt-10'>
+                                        <Button className='flex shadow-pop-br md:py-6 md:px-32 rounded-none lg:text-xl' color='pink' variant="gradient">Finalizar Orden</Button>
                                     </div>
                                 </div>
                             </div>
